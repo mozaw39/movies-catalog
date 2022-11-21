04af7a839651fb9c20e148ab28511be53119b626
Amanzou Anas <amenzouanas39@gmail.com>
2022-11-21 02:22:37 +0100

    first version of search and preview features

--- force-app/main/default/lwc/moviesResults/moviesResults.js
+++ force-app/main/default/lwc/moviesResults/moviesResults.js
@@ -1,20 +1,26 @@
-import { LightningElement } from "lwc";
-const imageSrc = 'https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/489x0/v2/https://resizing.flixster.com/VEqpmXsAtewTG2T4sLMh6G8K0YA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzZlODAyN2E4LTFhODctNGU0Mi04Y2M4LTMwMDI4MjBhODRiYy5qcGc=';
+import { api, LightningElement } from "lwc";
+const imageSrc =
+  "https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/489x0/v2/https://resizing.flixster.com/VEqpmXsAtewTG2T4sLMh6G8K0YA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzZlODAyN2E4LTFhODctNGU0Mi04Y2M4LTMwMDI4MjBhODRiYy5qcGc=";
 const MOVIES = [
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" },
-  { title: "title1", image: imageSrc, rating: 4, body:"body test" }
+  { title: "title2", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title2", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title2", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title1", image: imageSrc, rating: 4, body: "body test" },
+  { title: "title3", image: imageSrc, rating: 4, body: "body test" }
 ];
 
 export default class MoviesResults extends LightningElement {
-    iurl = imageSrc;
-    movies = MOVIES;
+  iurl = imageSrc;
+  movies = MOVIES;
+
+  @api
+  handleSearchResults(searchText) {
+    this.movies = MOVIES.filter((movie) => movie.title.includes(searchText));
+  }
 }
